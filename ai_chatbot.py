30a607b3d785df5f34f28467f386232f7c90232e
Evrd7 <eduardboitor@icloud.com>
2025-02-16 23:07:52 +0200

    Added render port

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -43,11 +43,12 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
     
-    app.run(debug=True)
 
 
-
+#app.run(debug=True)
 #port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
 #app.run(host="0.0.0.0", port=port, debug=True)
 
